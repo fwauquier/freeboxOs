d8bea003d1a3dfbb532590edb9b7012060199a5e
Frédéric Wauquier <frederic.wauquier@flyinggroup.aero>
2024-01-14 12:34:13 +0100

    Add some case to study from website call exploration

--- FreeboxOs.Tests/Study.cs
+++ FreeboxOs.Tests/Study.cs
@@ -3,6 +3,8 @@
 // <author > Frederic Wauquier</author >
 // </copyright >
 
+using FreeboxOs.lan;
+
 namespace FreeboxOs;
 
 [TestClass]
@@ -11,13 +13,53 @@ public class Study {
 
 
 	[TestMethod]
-	public async Task ParentalFilters() {
+	public async Task network_control() {
 		using var api = Settings.GetApi(TestContext);
 		await api.EnsureLoginAsync().ConfigureAwait(false);
 		var result = await api.GetAsync<object>("network_control").ConfigureAwait(false);
 		result.Dump(TestContext);
 	}
 
-
-
+	[TestMethod]
+	public async Task mac_filter() {
+		using var api = Settings.GetApi(TestContext);
+		await api.EnsureLoginAsync().ConfigureAwait(false);
+		var result = await api.GetAsync<object>("wifi/mac_filter").ConfigureAwait(false);
+		result.Dump(TestContext);
+	}
+	[TestMethod]
+	public async Task lan_browser_pub() {
+		using var api = Settings.GetApi(TestContext);
+		await api.EnsureLoginAsync().ConfigureAwait(false);
+		var result = await api.GetAsync<LanHost[]>("lan/browser/pub").ConfigureAwait(false);
+		result.Dump(TestContext);
+	}
+	[TestMethod]
+	public async Task wifi_ap() {
+		using var api = Settings.GetApi(TestContext);
+		await api.EnsureLoginAsync().ConfigureAwait(false);
+		var result = await api.GetAsync<object>("wifi/ap").ConfigureAwait(false);
+		result.Dump(TestContext);
+	}
+	[TestMethod]
+	public async Task wifi_config() {
+		using var api = Settings.GetApi(TestContext);
+		await api.EnsureLoginAsync().ConfigureAwait(false);
+		var result = await api.GetAsync<object>("wifi/config").ConfigureAwait(false);
+		result.Dump(TestContext);
+	}
+	[TestMethod]
+	public async Task wifi_bbs() {
+		using var api = Settings.GetApi(TestContext);
+		await api.EnsureLoginAsync().ConfigureAwait(false);
+		var result = await api.GetAsync<object>("wifi/bbs").ConfigureAwait(false);
+		result.Dump(TestContext);
+	}
+	[TestMethod]
+	public async Task taskcount() {
+		using var api = Settings.GetApi(TestContext);
+		await api.EnsureLoginAsync().ConfigureAwait(false);
+		var result = await api.GetAsync<object>("taskcount").ConfigureAwait(false);
+		result.Dump(TestContext);
+	}
 }
